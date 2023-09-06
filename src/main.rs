291ea52227def80f814569ea658f946db2452c36
cognivore <66186054+cognivore@users.noreply.github.com>
2023-09-06 17:08:14 +0100

    Smart 28 days calendar! Now I see the idea.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+pub fn months() -> Vec<&'static str> {
+    vec![
+        "Grimvold",
+        "Lymewald",
+        "Haggryme",
+        "Symswald",
+        "Harchment",
+        "Iggwyld",
+        "Chysting",
+        "Lillipythe",
+        "Haelhold",
+        "Reedwryme",
+        "Obthryme",
+        "Braghold",
+    ]
+}
+
+pub fn durations() -> Vec<i32> {
+    vec![30, 28, 30, 29, 29, 30, 31, 29, 28, 30, 28, 30]
+}
+
+pub fn days() -> Vec<&'static str> {
+    vec![
+        "Colly", "Chime", "Hayne", "Moot", "Frisk", "Eggdast", "Sunning",
+    ]
+}
+
+fn main() {
+    println!("Hello, world!");
+}
